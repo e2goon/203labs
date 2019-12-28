d2c4925eae37ebeb127c148ea5cbb5494fe81729
Hyowon <hyowon@Hyowonui-MacBookPro.local>
2019-12-28 20:34:04 +0900

    모바일 작업중

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,13 @@
 (function() {
+  var $btnHamburger = $('.btn-hamburger'),
+      $menu = $('.main-menu');
   $('.masonry-list').masonry({
     itemSelector: '.masonry-list-item',
     percentPosition: true
+  });
+
+  $btnHamburger.on('click', function(){
+    $menu.addClass('is-open');
   });
 
   AOS.init({
