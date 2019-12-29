d4a20ed72e1c8c239f651d5b192fa5334ca97063
wmlee <e2goon@gmail.com>
2019-12-29 17:27:29 +0900

    improve: 모바일 기능 개선

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,65 @@
 (function() {
-  var $body = $('body'),
-      $header = $('.header'),
-      $btnHamburger = $('.btn-hamburger');
-      
-  $('.masonry-list').masonry({
-    itemSelector: '.masonry-list-item',
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  var $body = $("body"),
+    $header = $(".header"),
+    $btnHamburger = $(".btn-hamburger");
+
+  $(".masonry-list").masonry({
+    itemSelector: ".masonry-list-item",
     percentPosition: true
   });
 
-  $btnHamburger.on('click', function(){
-    $header.toggleClass('is-open');
-    $body.toggleClass('menu-open');
+  $btnHamburger.on("click", function() {
+    $header.toggleClass("is-open");
+    $body.toggleClass("menu-open");
   });
 
-  AOS.init({
-    duration: 1000,
-    easing: '203labs'
+  $(".js-scroll").on("click", function(e) {
+    e.preventDefault();
+    var hash = this.hash;
+    var targetOffsetTop;
+    var offset = $(this).data("offset") || 0;
+    if (hash !== "") {
+      targetOffsetTop = $(hash).offset().top;
+    } else {
+      targetOffsetTop = 0;
+    }
+    $("html, body")
+      .stop()
+      .animate({ scrollTop: targetOffsetTop + offset }, 1000, "swing");
   });
+
+  $(window).on("DOMContentLoaded", function() {
+    if (window.innerWidth < 768) {
+      AOS.init({
+        duration: 1000,
+        easing: "203labs",
+        anchorPlacement: "top-center"
+      });
+    } else {
+      AOS.init({
+        duration: 1000,
+        easing: "203labs"
+      });
+    }
+  });
+
+  $(window).on("resize", debounce(function() {
+    $(".masonry-list").masonry();
+  }, 1000));
 })();
